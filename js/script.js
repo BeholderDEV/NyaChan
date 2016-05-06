1fa4eca1a0bf5a33a57e23e1ac8dd960d7d50f95
Alisson Steffens <alisson.steffens@hotmail.com>
2016-05-06 16:32:24 -0300

    imagem de cabeçalho

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('#hero').css({
-    'height': $(window).height()+'px'
-    })
-});
+//$(document).ready(function(){
+//    $('#hero').css({
+//    'height': $(window).height()+'px'
+//    })
+//});
