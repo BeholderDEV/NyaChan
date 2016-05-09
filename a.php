e4b3b42305a7eefcd5479ce45574c007943950cb
Alisson Steffens <alisson.steffens@hotmail.com>
2016-05-09 14:01:59 -0300

    getting pages

--- a.php
+++ a.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
@@ -20,97 +22,109 @@
                 <h1>Anime</h1>
             </div>
             <?php
-            for($i=1; $i<3;$i++){
-                $jsonurl = "http://a.4cdn.org/a/".$i.".json";
-                $json = file_get_contents($jsonurl);
-                $json_output = json_decode($json);
+                $board ="a";
+                $page = 1;
+                $fim =1;
+                if (isset($_GET["page"])){
+                    if("all" === $_GET["page"]){
+                        $fim = 10;
+                    }
+                    else{
+                        $page = $_GET["page"];
+                        $fim = $page;
+                    }
+                }
+                
+                for($i=$page; $i<=$fim;$i++){
+                    $jsonurl = "http://a.4cdn.org/".$board."/".$i.".json";
+                    $json = file_get_contents($jsonurl);
+                    $json_output = json_decode($json);
 
-                foreach ($json_output->threads as $thread) {
-                    foreach($thread->posts as $post) {
-                        if (isset($post->bumplimit)) {
-                            
-                            if (isset($post->sub)){
-                                $sub  = $post->sub;    
-                            }
-                            else{
-                                $sub  = "";    
-                            }
-                            $no  = $post->no;
-                            if (isset($post->com)){
-                                $com  = $post->com;
-                                
-                            }
-                            else{
-                                $com  = "";    
-                            }
-                            $ext  = $post->ext;
-                            $name  = $post->name;
-                            $tim  = $post->tim;
-                            
-                            
-                            
-                            echo "<div class='panel panel-default' id='".$no."'><div class='panel-heading'><h3 class='panel-title'>".$sub."<small>".$no." by ".$name."</small></h3></div><div class='panel-body'>";
-                            
-                            if('.webm' === $ext){
-                                echo "<video controls width='320' height='240' autoplay><source src='http://i.4cdn.org/a/".$tim.$ext."'type='video/webm' codecs='vp8, vorbis'></video>";
+                    foreach ($json_output->threads as $thread) {
+                        foreach($thread->posts as $post) {
+                            if (isset($post->bumplimit)) {
 
-                            }
-                            else{
-                                echo "<img src='http://i.4cdn.org/a/".$tim.$ext."' class='img-responsive' width='150px'>";
-                            }
-                            
-                            echo $com."</div><div class='panel-footer'>";
-                            echo "<ul class='list-group'>";
-                            /*
-                            if (strpos($sub, 'DOTA') !== false) {
-                                echo 'Found DOTA!!! Thread Number is: ' . $thread->no;
-                            } 
-                            */
-                        }
-                        else{
-                            $hasImage=true;
-                            $no  = $post->no;
-                            if (isset($post->com)){
-                                $com  = $post->com;
-                                
-                            }
-                            else{
-                                $com  = "";    
-                            }
-                            if (isset($post->ext)){
-                               $ext  = $post->ext;
-                            }
-                            else{
-                                $ext  = "";
-                                $hasImage=false;
-                            }
-                            if (isset($post->tim)){
-                               $tim  = $post->tim;   
-                            }
-                            else{
-                                $tim  = "";
-                                $hasImage=false;
-                            }
-                            $name  = $post->name;
-                            
-                            echo "<li class='list-group-item'>";
-                            if($hasImage){
-                                if('.webm' === $ext){
-                                    echo "<video controls width='320' height='240' autoplay><source src='http://i.4cdn.org/a/".$tim.$ext."'type='video/webm' codecs='vp8, vorbis'></video>";
-                                    
+                                if (isset($post->sub)){
+                                    $sub  = $post->sub;    
                                 }
                                 else{
-                                    echo "<img src='http://i.4cdn.org/a/".$tim.$ext."' class='img-responsive' width='150px'>";
+                                    $sub  = "";    
                                 }
-                                
-                            }                            
-                            echo $com."</li>";
+                                $no  = $post->no;
+                                if (isset($post->com)){
+                                    $com  = $post->com;
+
+                                }
+                                else{
+                                    $com  = "";    
+                                }
+                                $ext  = $post->ext;
+                                $name  = $post->name;
+                                $tim  = $post->tim;
+
+
+
+                                echo "<div class='panel panel-default' id='".$no."'><div class='panel-heading'><h3 class='panel-title'>".$sub."<small>".$no." by ".$name."</small></h3></div><div class='panel-body'>";
+
+                                if('.webm' === $ext){
+                                    echo "<video controls width='320' height='240' autoplay><source src='http://i.4cdn.org/".$board."/".$tim.$ext."'type='video/webm' codecs='vp8, vorbis'></video>";
+
+                                }
+                                else{
+                                    echo "<img src='http://i.4cdn.org/".$board."/".$tim.$ext."' class='img-responsive' width='150px'>";
+                                }
+
+                                echo $com."</div><div class='panel-footer'>";
+                                echo "<ul class='list-group'>";
+                                /*
+                                if (strpos($sub, 'DOTA') !== false) {
+                                    echo 'Found DOTA!!! Thread Number is: ' . $thread->no;
+                                } 
+                                */
+                            }
+                            else{
+                                $hasImage=true;
+                                $no  = $post->no;
+                                if (isset($post->com)){
+                                    $com  = $post->com;
+
+                                }
+                                else{
+                                    $com  = "";    
+                                }
+                                if (isset($post->ext)){
+                                   $ext  = $post->ext;
+                                }
+                                else{
+                                    $ext  = "";
+                                    $hasImage=false;
+                                }
+                                if (isset($post->tim)){
+                                   $tim  = $post->tim;   
+                                }
+                                else{
+                                    $tim  = "";
+                                    $hasImage=false;
+                                }
+                                $name  = $post->name;
+
+                                echo "<li class='list-group-item'>";
+                                if($hasImage){
+                                    if('.webm' === $ext){
+                                        echo "<video controls width='320' height='240' autoplay><source src='http://i.4cdn.org/".$board."/".$tim.$ext."'type='video/webm' codecs='vp8, vorbis'></video>";
+
+                                    }
+                                    else{
+                                        echo "<img src='http://i.4cdn.org/".$board."/".$tim.$ext."' class='img-responsive' width='150px'>";
+                                    }
+
+                                }                            
+                                echo $com."</li>";
+                            }
                         }
+                        echo "</ul></div></div>";
                     }
-                    echo "</ul></div></div>";
                 }
-            }
-                
             ?>
         </div>        
         
