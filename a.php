2249f88bb730d10e88ead4ab458239094448b724
Alisson Steffens <alisson.steffens@hotmail.com>
2016-05-20 17:06:48 -0300

    novo layout

--- a.php
+++ a.php
@@ -10,6 +10,7 @@
 
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/css/jasny-bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <!-- Meu CSS -->
         <link href="css/estilo.css" rel="stylesheet" type="text/css">
@@ -17,26 +18,31 @@
     </head>
     <body>
         
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Nya Chan</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="btn" data-toggle="modal" data-target="#myModal"><a>New Thread  <i class="fa fa-plus"></i></a></li>
+                        <li class="btn"><a><i class="fa fa-user"></i> Alisson</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+        
+        <div id="back-to-top-button">
+            <a href="javascript:void(0);" title="Voltar ao início da página">
+                <span class="glyphicon glyphicon-chevron-up"></span>
+            </a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="../navbar/">Default</a></li>
-                <li><a href="../navbar-static-top/">Static top</a></li>
-                <li><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
-            </ul>
-        </div><!--/.nav-collapse -->
-    </div>
-</nav>
         
         <div class="container">
             
@@ -66,7 +72,6 @@
                     foreach ($json_output->threads as $thread) {
                         foreach($thread->posts as $post) {
                             if (isset($post->bumplimit)) {
-
                                 if (isset($post->sub)){
                                     $sub  = $post->sub;    
                                 }
@@ -80,6 +85,12 @@
                                 }
                                 else{
                                     $com  = "";    
+                                }
+                                if (isset($post->trip)){
+                                    $trip  = $post->trip;    
+                                }
+                                else{
+                                    $trip = "";    
                                 }
                                 $ext  = $post->ext;
                                 $name  = $post->name;
@@ -101,7 +112,7 @@
                                 
                                 
                                 
-                                echo "<h3 class='panel-title'>".$sub." ~ <span> No.<a href='#".$no."'>".$no."</a> by ".$name."</span></h3>";
+                                echo "<h3 class='panel-title'>".$sub." ~ <span> No.<a href='#".$no."'>".$no."</a> by ".$name." ".$trip."</span></h3>";
                                 
                                 echo "</div>";
                                 
@@ -116,10 +127,11 @@
                                     echo "<img src='http://i.4cdn.org/".$board."/".$tim.$ext."' class='img-responsive main-image image-md'>";
                                 }
 
-                                echo $com."</div><div class='panel-footer'>";
+                                echo "<p>".$com."</p></div><div class='panel-footer'>";
                                 echo "<ul class='list-group'>";
                             }
                             else{
+                                
                                 $hasImage=true;
                                 $no  = $post->no;
                                 if (isset($post->com)){
@@ -143,7 +155,13 @@
                                     $tim  = "";
                                     $hasImage=false;
                                 }
-
+                                $name = $post->name;
+                                
+                                if(isset($post->capcode)){
+                                    echo "<div class='panel'><div class='panel-footer'>";
+                                    echo "<ul class='list-group'>";
+                                }
+                                
                                 echo "<li class='list-group-item'>";
                                 echo "<div class='zero-clipboard'><span class='com-btn-clipboard'>";
 
@@ -161,7 +179,7 @@
                                     }
 
                                 }                            
-                                echo $com."</li>";
+                                echo "<p>".$com."</p></li>";
                             }
                         }
                         echo "</ul></div></div>";
@@ -171,11 +189,61 @@
         </div>        
         
         
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">New Thread</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-2 control-label">Subject</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputEmail3" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-2 control-label">Comment</label>
+                                <div class="col-sm-10">
+                                    <textarea class="form-control" rows="3"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-2 control-label">File</label>
+                                <div class="col-sm-10">
+                                    <div class="fileinput fileinput-new" data-provides="fileinput">
+                                        <span class="btn btn-default btn-file"><span>Choose file</span><input type="file" multiple /></span>
+                                        <span class="fileinput-filename"></span><span class="fileinput-new">No file chosen</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-nya">Post</button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+        
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <!-- Bootstrap -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
         <script>
+            $("#back-to-top-button").click(function() {
+                $('html,body').animate({scrollTop: $("body").offset().top},'slow');
+            });
+            $(document).on('change', '.btn-file :file', function() {
+                var input = $(this),
+                    numFiles = input.get(0).files ? input.get(0).files.length : 1,
+                    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+                input.trigger('fileselect', [numFiles, label]);
+            });
             $(".main-image").click(function(){
                 $(this).toggleClass( "image-md" );
             });
