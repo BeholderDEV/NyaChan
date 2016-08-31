a0cbb7987470132ff859f9f65c979a293e175b58
AlissonSteffens <alisson.steffens@hotmail.com>
2016-08-30 21:06:24 -0300

    directives?

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 (function(){
     var app = angular.module('nya-chan',[]);
     
-    app.controller('HabilityController',function(){
-
+    app.controller('threadController',function(){
+        $scope.threads = [];  
+        $scope.search = function() {        
+            $http({method: 'JSONP', url: "https://raw.githubusercontent.com/BeholderDEV/NyaChan/master/js/app.js"}).
+                success(function(data, status) {
+                $scope.threads = data.entries;
+            }).
+            error(function(data, status) {
+                console.log(data || "Request failed");
+            });
+        };
     });
     
 })();
