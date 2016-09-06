80b8352a96ef587e64998646f50ee7f30bc2cd15
AlissonSteffens <alisson.steffens@hotmail.com>
2016-09-06 19:00:43 -0300

    thread nao funciona

--- js/app.js
+++ js/app.js
@@ -18,20 +18,28 @@
         };
         $scope.threads = $scope.search();
 
-        $scope.searchThread = function() {
-            console.log("entrou na função");
-            $http({
-                method : "GET",
-                //url : "http://127.0.0.1:3000/a/thread/1"
-                url: "https://nyachan-server.herokuapp.com/a/thread/1"
-            }).then(function mySucces(response) {
-                $scope.threads = JSON.parse(response.data);
-                console.log($scope.threads);
-            }, function myError(response) {
-                  console.log(response || "Request failed");
-            });
-        };
-        $scope.thread = $scope.searchThread();
     });
+
+    app.controller('animeThreadController',function($scope, $http){
+
+      $scope.searchThread = function() {
+          console.log("entrou na função");
+          $http({
+              method : "GET",
+              //url : "http://127.0.0.1:3000/a/thread/1"
+              url: "https://nyachan-server.herokuapp.com/a/thread/1"
+          }).then(function mySucces(response) {
+              $scope.threads = JSON.parse(response.data);
+              console.log($scope.threads);
+          }, function myError(response) {
+                console.log(response || "Request failed");
+          });
+      };
+      $scope.thread = $scope.searchThread();
+
+    });
+
+
+
 
 })();
