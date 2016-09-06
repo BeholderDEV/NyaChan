abfd7ea79a5ee3d970512eef588ff0a9cbbbb5ea
AlissonSteffens <alisson.steffens@hotmail.com>
2016-09-06 13:28:06 -0300

    renderizando pagina

--- NyachanServer/RestAPI.js
+++ NyachanServer/RestAPI.js
@@ -11,6 +11,7 @@ app.use(function(req, res, next) {
 app.get('/a/threads', function (req, res) {
    res.type('application/json');
    fs.readFile( __dirname + "/" + "tag_anime.json", 'utf8', function (err, data) {
+     console.log(data);
       res.jsonp( data );
    });
 })
