62843b30c4718d023303f2828e01c459b1ee14c1
Augus-top <acpluschkat@hotmail.com>
2016-10-10 09:47:20 -0300

    Post a Post (Sempre o mesmo)

--- NyachanServer/RestAPI.js
+++ NyachanServer/RestAPI.js
@@ -1,46 +1,26 @@
 var express = require('express');
-var app = express();
-var fs = require("fs");
+var bodyParser = require('body-parser')
 var mongodb = require('mongodb');
+var fs = require("fs");
 var MongoClient = mongodb.MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var app = express();
 
 // Connection URL
 var url = 'mongodb://alisson:123456@ds053206.mlab.com:53206/nyachan_data';
 
-//    MongoClient.connect(url, function(err, db) {
-//        if (err) {
-//        console.log('Unable to connect to the mongoDB server. Error:', err);
-//        } else {
-//        console.log('Connection established to', url);
-//
-//        var cursor = db.collection('thread').find({ "tags": "Anime" });
-//        
-//        cursor.each(function(err, doc) {
-//            if (doc != null) {
-//                console.dir(doc);
-//            } else {
-//                console.log("SHIT");
-//            }
-//        });
-//            
-//        db.close();
-//        }
-//    });
-
-
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   next();
 });
 
+app.use(bodyParser.json());
+
+
+
 app.get('/a/threads', function (req, res) {
-//   res.type('application/json');
-//   fs.readFile( __dirname + "/" + "tag_anime.json", 'utf8', function (err, data) {
-//     console.log(data);
-//      res.jsonp( data );
-//   });
     MongoClient.connect(url, function(err, db) {
         if (err) {
         console.log('Unable to connect to the mongoDB server. Error:', err);
@@ -49,7 +29,7 @@ app.get('/a/threads', function (req, res) {
         db.collection('thread').find( { } ).toArray(function(error, documents) {
             if (err){
                 throw error;
-            } 
+            }
             res.jsonp(documents);
         });
             
@@ -60,11 +40,21 @@ app.get('/a/threads', function (req, res) {
 })
 
 app.get('/a/thread/1', function (req, res) {
-   res.type('application/json');
-   fs.readFile( __dirname + "/" + "tag_anime_thread.json", 'utf8', function (err, data) {
-     console.log(data);
-      res.jsonp( data );
-   });
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+        console.log('Connection established to', url);
+        db.collection('thread').find( { _id: ObjectId("57fb3a3bdcba0f6a8b60f17a")  } ).toArray(function(error, documents) {
+            if (err){
+                throw error;
+            }
+            res.jsonp(documents);
+        });
+            
+        db.close();
+        }
+    });
 })
 
 app.get('/', function (req, res) {
@@ -83,6 +73,31 @@ app.get('/css/estilo.css', function (req, res) {
 app.get('/js/app.js', function (req, res) {
    res.type('text/javascript');
    res.sendfile('js/app.js');
+})
+
+app.put('/a/thread/newPost', function (req, res){
+    var newPost = req.body;
+    console.log(newPost);
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+        console.log('Connection established to', url);
+        
+        db.collection('thread', function(err, collection) {
+            collection.update({'_id': ObjectId(newPost.threadid)},{ $push: {post: newPost}}, {safe:true}, function(err, result) {
+                if (err) {
+                    console.log('Error ' + err);
+                    res.send({'error':'An error has occurred'});
+                } else {
+                    console.log('' + result);
+                    res.send(newPost);
+                }
+            });
+        });
+        db.close();
+        }
+    });
 })
 
 var port = process.env.PORT || 3000;
--- js/app.js
+++ js/app.js
@@ -21,25 +21,47 @@
     });
 
     app.controller('animeThreadController',function($scope, $http){
-
       $scope.searchThread = function() {
-          console.log("entrou na função");
           $http({
               method : "GET",
-              //url : "http://127.0.0.1:3000/a/thread/1"
+//              url : "http://127.0.0.1:3000/a/thread/1"
               url: "https://nyachan-server.herokuapp.com/a/thread/1"
           }).then(function mySucces(response) {
-              $scope.thread = JSON.parse(response.data);
+              $scope.thread = response.data[0];
               console.log($scope.thread);
           }, function myError(response) {
                 console.log(response || "Request failed");
           });
       };
       $scope.thread = $scope.searchThread();
-
+        
+      $scope.addPost = function(){
+          var dataPost = {
+                id: "123123123",
+                threadid: $scope.thread._id,
+                body: "Look at this shit",
+                date: "2016-01-02 19:33:00",
+                tile: "Help",
+                userName: "Anon"
+          };
+          $http({
+              method : "PUT",
+//              url : "http://127.0.0.1:3000/a/thread/newPost",
+              url: "https://nyachan-server.herokuapp.com/a/thread/newPost",
+              data: dataPost,
+              headers: {
+                    'Content-Type': 'application/json'
+              }
+          }).then(function mySucces(response) {
+                console.log(response.data);
+          }, function myError(response) {
+                console.log(response || "Request failed");
+          });
+      }
+    
     });
 
-
+    
 
 
 })();
