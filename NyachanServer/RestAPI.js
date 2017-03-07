a7ca9c95de636be70181af494339002f16644d4f
AlissonSteffens <alisson.steffens@hotmail.com>
2017-03-06 21:05:57 -0300

    arquivo faltante

--- NyachanServer/RestAPI.js
+++ NyachanServer/RestAPI.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var path = require('path');
 var fs = require("fs");
---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.panel-body').on('click', '.OP', function(){
+    $(this).toggleClass('image-thumb-OP');
+});
+$('.panel-footer').on('click', '.post-image', function(){
+    $(this).toggleClass('image-thumb');
+});
