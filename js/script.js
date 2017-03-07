7d32bd8a0eba9f7dfac1ac9181b1eaf6123a528f
AlissonSteffens <alisson.steffens@hotmail.com>
2017-03-06 21:20:02 -0300

    começando do cemeço

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $('.panel-body').on('click', '.OP', function(){
     $(this).toggleClass('image-thumb-OP');
 });
+
 $('.panel-footer').on('click', '.post-image', function(){
     $(this).toggleClass('image-thumb');
 });
