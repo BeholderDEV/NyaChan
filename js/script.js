4cc9ddf83b95c6e5692a1e147cd91978e91ef2c0
AlissonSteffens <alisson.steffens@hotmail.com>
2017-03-13 21:18:17 -0300

    morre adson

--- js/script.js
+++ js/script.js
@@ -5,10 +5,3 @@ $('body').on('click', '.panel-body .OP', function(){
 $('body').on('click', '.panel-footer .post-image', function(){
     $(this).toggleClass('image-thumb');
 });
-
-$('#newThreadModal').on('shown.bs.modal', function (e) {
-  $(this).find('form').validator()
-  $('#newThreadModal').on('hidden.bs.modal', function (e) {
-    $(this).find('form').off('submit').validator('destroy')
-  })
-})
