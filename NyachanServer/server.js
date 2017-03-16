6caa5ae0e9e9a47799ee62daf270612a42d49f1f
AdsonEsteves <shinadson@gmail.com>
2017-03-16 18:52:29 -0300

    erro no pc do alisson

--- NyachanServer/server.js
+++ NyachanServer/server.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 var app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://nyachan-server.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   next();
