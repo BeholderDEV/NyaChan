90f6ee3f910471ed35a347dd68814873959a4bdf
Unknown <acpluschkat@hotmail.com>
2017-03-20 21:19:53 -0300

    Test safe disable

--- NyachanServer/databaseManager.js
+++ NyachanServer/databaseManager.js
@@ -76,7 +76,7 @@ module.exports = function(app){
 		        console.log('Connection established to', url);
 
 		        db.collection('thread', function(err, collection) {
-		            collection.update({'_id': ObjectId(newPost.threadid)},{ $push: {post: newPost}}, {safe:true}, function(err, result) {
+		            collection.update({'_id': ObjectId(newPost.threadid)},{ $push: {post: newPost}}, function(err, result) {
 		                if (err) {
 		                    console.log('Error ' + err);
 		                    res.send({'error':'An error has occurred'});
