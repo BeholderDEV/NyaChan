840e4535e0dc8e67d58036f29fbb2cacf6205d98
Unknown <acpluschkat@hotmail.com>
2017-03-27 20:25:59 -0300

    omg augustop

--- NyachanServer/databaseManager.js
+++ NyachanServer/databaseManager.js
@@ -35,16 +35,21 @@ module.exports = function(app){
 	        	console.log('Unable to connect to the mongoDB server. Error:', err);
 	        } else {
 		        console.log('Connection established to', url);
-		        var data_thread = db.collection('thread').find( { _id: ObjectId(req.params.idThread)  }, function(error, documents) {
+		        var data_thread = db.collection('thread').find( { _id: ObjectId(req.params.idThread)  }, function(error, cursor) {
 		            if (error){
 									console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA MORRI");
 		                res.status(404).send("Not Found");
 		            }
 								console.log("BBBBBBBBBBBBB");
-
 		        } );
 
-						res.jsonp(data_thread.toArray());
+						data_thread.toArray(function(error, documents) {
+		            if (error){
+									console.log("cccccccccc");
+		            }
+								console.log("dddddddddddddddddddddddddddddddddddddddddd");
+		            res.jsonp(documents);
+		        } ););
 
 		        db.close();
 	        }
