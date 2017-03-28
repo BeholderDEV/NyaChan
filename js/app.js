1c092cee0182093f83ccf10b9fcace00f03bf604
AlissonSteffens <alisson.steffens@hotmail.com>
2017-03-27 22:51:15 -0300

    omg sorry

--- js/app.js
+++ js/app.js
@@ -157,7 +157,7 @@
 
 // THREAD
 
-    app.controller('threadController',function($scope, $http){
+    app.controller('threadController',function($scope, $http, $location){
       $scope.time_zone = new Date().getTimezoneOffset();
       $scope.response = null;
       $scope.widgetId = null;
@@ -179,7 +179,7 @@
               $scope.thread = response.data[0];
           }, function myError(response) {
               console.log("aqui")
-              $location.path('/newValue')
+              $location.path('/404')
           });
       };
 
