f6ba1594addb347cb3593c1aa7d8304ddeb93579
Unknown <acpluschkat@hotmail.com>
2017-03-27 21:16:20 -0300

    redir 2

--- NyachanServer/databaseManager.js
+++ NyachanServer/databaseManager.js
@@ -46,7 +46,7 @@ module.exports = function(app){
 						    console.error("AAAAAAA" + err);
 								// res.writeHead(404, {location: 'https://nyachan-server.herokuapp.com/app/tag/a'});
 								// res.end();
-								res.redirect(404,'https://nyachan-server.herokuapp.com/404');
+								res.redirect(404,'/404');
 						}
 		        db.close();
 	        }
