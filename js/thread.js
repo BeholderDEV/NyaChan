8b4d31d23b60502ea54b81b126f733e52cf2ad5c
AdsonEsteves <shinadson@gmail.com>
2017-04-07 17:58:30 -0300

    só vou aceitar

--- js/thread.js
+++ js/thread.js
@@ -72,8 +72,6 @@
                     }
         }).then(function successCallback(response) {
             console.log(response.data.body);
-						console.log(response.data.body.success);
-						console.log(response.data.body[0].success);
           	valid=response.data.body;
             validatedPost(valid);
         }, function errorCallback(response) {
