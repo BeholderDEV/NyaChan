a670afc3c0f3a45e650d1f1349ef004478b2958c
AlissonSteffens <alisson.steffens@hotmail.com>
2017-04-17 20:18:32 -0300

    nooooo

--- js/thread.js
+++ js/thread.js
@@ -178,8 +178,8 @@
 										$('#newThreadModal').modal('hide');
 										$('#loader').width('0%');
 										var alertHtml = $("#alert-model").html();
-					          alertHtml = cardHtml.replace('${kind}', 'success');
-					          alertHtml = cardHtml.replace('${mensagem}', 'postado com sucesso');
+					          alertHtml = alertHtml.replace('${kind}', 'success');
+					          alertHtml = alertHtml.replace('${mensagem}', 'postado com sucesso');
 										$('body').append(alertHtml)
 								}, function myError(response) {
 										console.log(response || "Request failed");
