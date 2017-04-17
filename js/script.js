d9f7b3a9d4e684cd131cdfe898bc589767c79e73
AlissonSteffens <alisson.steffens@hotmail.com>
2017-04-17 19:34:16 -0300

    uncloaked

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function restore_image(){
 }
 $(".some-image").click(function(){
     var img = $(this).data('full');
-    var full = '<img src="'+img+'" class="img-responsive nya-image-full" alt="loading"/>';
+    var full = '<img src="'+img+'" class="img-responsive nya-image-full"/>';
     var tag = $(full);
     $(tag).on("click", restore_image);
     var w =  $(this).attr('width');
