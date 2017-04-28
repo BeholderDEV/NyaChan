c0425afa4019e7da7092d8924eca9d50c97bdab9
AdsonEsteves <shinadson@gmail.com>
2017-04-28 15:04:51 -0300

    faltou em 1

--- NyachanServer/databaseManager.js
+++ NyachanServer/databaseManager.js
@@ -78,7 +78,7 @@ module.exports = function(app, passport){
 	        	console.log('Unable to connect to the mongoDB server. Error:', err);
 	        } else {
 		        console.log('Connection established to', url);
-		        db.collection('thread').find( { tags: req.params.tagName, archived : false} ).toArray(function(error, documents) {
+		        db.collection('thread').find( { tags: req.params.tagName, archived: false} ).toArray(function(error, documents) {
 		            if (error){
 		                throw error;
 		            }
@@ -99,7 +99,7 @@ module.exports = function(app, passport){
 						console.log('Unable to connect to the mongoDB server. Error:', err);
 					} else {
 						console.log('Connection established to', url);
-						db.collection('thread').find( { tags: req.params.tagName} ).sort(query).toArray(function(error, documents) {
+						db.collection('thread').find( { tags: req.params.tagName, archived: false} ).sort(query).toArray(function(error, documents) {
 								if (error){
 										throw error;
 								}
