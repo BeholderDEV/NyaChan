010fc43e806ffe2ac7fa9aff88176acd71db6cb3
AlissonSteffens <alisson.steffens@hotmail.com>
2017-04-28 21:48:03 -0300

    archiveds?

---
+++ js/archived.js
@@ -0,0 +1,21 @@
+(function(){
+
+	var app = angular.module('nya-chan', ['angular-loading-bar', 'vcRecaptcha'])
+    .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+      cfpLoadingBarProvider.includeSpinner = false;
+    }]);
+
+    app.controller('archivedController',function($scope, $http){
+      ar url = $(location).attr('href');
+      var searchTag = url.substring(url.lastIndexOf('/') + 1);
+      $http({
+          method : "GET",
+          url: "https://nyachan-server.herokuapp.com/api/threads/" + searchTag+"?sortType=lastDate&archived=true"
+      }).then(function mySucces(response) {
+          $scope.threads = response.data;
+      }, function myError(response) {
+          $window.location.href="https://nyachan-server.herokuapp.com/404";
+      });
+    });
+
+})();
