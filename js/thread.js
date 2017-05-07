c6c69994d99ad71bb7d18ebdad18108a56b05c82
Augus-top <acpluschkat@hotmail.com>
2017-05-07 18:21:35 -0300

    Bug cookie logout

--- js/thread.js
+++ js/thread.js
@@ -248,7 +248,7 @@
         }
       }).then(function mySucces (response) {
         $scope.isUserLogged = false
-        $cookieStore.remove('user')
+        $cookies.remove($cookies.get('user'));
         toastr.success('Goodbye', 'See you soon')
       }, function myError (response) {
         console.log(response || 'Request failed')
