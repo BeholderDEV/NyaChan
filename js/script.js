4a8e9399aa2d0008a4ecca677864c15a9e8ca23a
AlissonSteffens <alisson.steffens@hotmail.com>
2017-05-08 15:35:36 -0300

    on modal

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function toggle (obj) {
   $('#image-name').append(nam)
   $('#carousel-modal').empty()
   $('#carousel-modal').append(full)
-  $('#carousel-modal').on('show.bs.modal', function () {
+  $('#showImageModal').on('show.bs.modal', function () {
          $(this).find('.modal-body').css({
                 width:'auto', //probably not needed
                 height:'auto', //probably not needed
