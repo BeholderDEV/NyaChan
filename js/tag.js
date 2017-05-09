be6bfac9e2f2dd7d2aff5bf0ce0de304a12a228e
AdsonEsteves <shinadson@gmail.com>
2017-05-09 15:35:44 -0300

    1 seg

--- js/tag.js
+++ js/tag.js
@@ -263,7 +263,7 @@
               vcRecaptchaService.reload($scope.widgetId)
               $('#newThreadModal').modal('hide')
               $('#loader').width('0%')
-            }, 500)
+            }, 1000)
             toastr.success('Nice Thread created', 'Success')
           }, function myError (response) {
             console.log(response || 'Request failed')
