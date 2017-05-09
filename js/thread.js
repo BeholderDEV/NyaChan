fd9398268b4facf7a4ed200dc7f63c4b11af44fb
AdsonEsteves <shinadson@gmail.com>
2017-05-09 15:40:22 -0300

    time

--- js/thread.js
+++ js/thread.js
@@ -208,6 +208,7 @@
                 var uploadedFile = JSON.parse(xhr.response)
                 uploadedFiles[i] = uploadedFile
                 if (i >= files.length - 1) {
+                  $('#loader').width('90%')
                   sendPost(files, uploadedFiles)
                 } else {
                   sendFilesToDropbox(i + 1, files, uploadedFiles)
@@ -218,7 +219,7 @@
               if (evt.lengthComputable) {
                 var percentComplete = evt.loaded / evt.total
                 percentComplete = (percentComplete / files.length) * (i + 1)
-                $('#loader').width(Math.round(percentComplete * 90) + '%')
+                $('#loader').width(Math.round(percentComplete * 75) + '%')
               }
             }, false)
             xhr.open('post', '/dbxPost/0/' + $scope.thread._id, true)
@@ -264,7 +265,7 @@
               vcRecaptchaService.reload($scope.widgetId)
               $('#newThreadModal').modal('hide')
               $('#loader').width('0%')
-            }, 1000)
+            }, 2000)
             toastr.success('Right in the Post', 'You\'ve just answered it')
           }, function myError (response) {
             toastr.error('OMG, its dead!', 'Error')
