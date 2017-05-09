eed1b57f40cbd4203cd99f770c4f13d2e28315f7
AdsonEsteves <shinadson@gmail.com>
2017-05-09 15:53:14 -0300

    1500

--- js/thread.js
+++ js/thread.js
@@ -265,7 +265,7 @@
               vcRecaptchaService.reload($scope.widgetId)
               $('#newThreadModal').modal('hide')
               $('#loader').width('0%')
-            }, 2000)
+            }, 1500)
             toastr.success('Right in the Post', 'You\'ve just answered it')
           }, function myError (response) {
             toastr.error('OMG, its dead!', 'Error')
