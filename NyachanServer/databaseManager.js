da99bf18b249641cf19304d606d7465d80e9289a
AlissonSteffens <alisson.steffens@hotmail.com>
2017-05-12 18:32:57 -0300

    Inspect

--- NyachanServer/databaseManager.js
+++ NyachanServer/databaseManager.js
@@ -49,7 +49,7 @@ module.exports = function (app, passport) {
                 if (err) {
                   console.log('Error ' + err)
                 }
-                console.log("HERE " + result)
+                console.log("HERE " + util.inspect(myObject, false, null))
                 // var post
                 // result.post.forEach(function(t) {  /// Arrumar forma melhor de fazer isso
                   // if (ObjectId(t.idPost) === ObjectId(req.body.postId)) {
